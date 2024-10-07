d3a07209675e678fcd3afe69776bb62aa46ee820
arex123 <ad47kumar@gmail.com>
2024-10-07 21:20:21 +0530

    initiated, expenseitem component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+// Write your code here:
+function ExpenseItem(){
+  return <>
+    <div>
+      15 August 2023
+    </div>
+    <div>
+      <p>Amount</p>
+      <p>Book</p>
+      <p>$10</p>
+    </div>
+    <div>
+      <p>Location</p>
+      <p>Delhi</p>
+    </div>
+    
+  </>
+}
+export default ExpenseItem
